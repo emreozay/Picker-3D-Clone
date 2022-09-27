d4ef1a777ad79535977a187a0f3ef2b022a6c34b
Emre Özay <emreozay@mail.com>
2022-09-27 15:48:20 +0300

    +gate prefab and script for gate movement added

---
+++ Assets/Scripts/GateMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateMovement : MonoBehaviour
+{
+    private float signOfAngle;
+    private float angle;
+
+    void Start()
+    {
+        signOfAngle = Mathf.Sign(transform.localPosition.x);
+
+        if (signOfAngle == 1)
+            angle = 60f;
+        else
+            angle = 300f;
+    }
+
+    void Update()
+    {
+        if (transform.localRotation.eulerAngles.y * signOfAngle < angle * signOfAngle || transform.localRotation.eulerAngles.y == 0f)
+            transform.RotateAround(transform.parent.position, Vector3.forward, 20 * signOfAngle * Time.deltaTime);
+    }
+}
