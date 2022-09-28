1b6a1278a091f51a300f809c29797e18bb2fc129
Emre Özay <emreozay@mail.com>
2022-09-28 22:24:08 +0300

    +refactor: movement code of collectible objects moved to another script

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -4,11 +4,6 @@ public class ObstacleMovement : MonoBehaviour
 {
     private void Start()
     {
-        ContainerControl.containerStop += AddForceToObstacles;
-    }
 
-    private void AddForceToObstacles()
-    {
-        GetComponent<Rigidbody>().AddForce(Vector3.forward * 3f, ForceMode.Impulse);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 0.5f;
+    [SerializeField] private float forceSpeed = 3f;
     private float lastMousePoint;
     private bool isMouseDown = false;
     private Rigidbody rb;
     private bool isStop = false;
+    private List<GameObject> collectedObjects;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        collectedObjects = new List<GameObject>();
 
         ContainerControl.containerStop += StopMovement;
         ContainerControl.gatesUp += ContinueMovement;
@@ -70,10 +74,31 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Collectable"))
+        {
+            collectedObjects.Add(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Collectable"))
+        {
+            collectedObjects.Remove(collision.gameObject);
+        }
+    }
+
     private void StopMovement()
     {
         transform.GetChild(0).gameObject.SetActive(false);
         isStop = true;
+
+        for (int i = 0; i < collectedObjects.Count; i++)
+        {
+            collectedObjects[i].GetComponent<Rigidbody>().AddForce(Vector3.forward * forceSpeed, ForceMode.Impulse);
+        }
     }
 
     private void ContinueMovement()
