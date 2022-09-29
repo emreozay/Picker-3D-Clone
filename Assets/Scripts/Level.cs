c5103741ef58eb2b96f7bf0fbabfe9f27953a1a2
Emre Özay <emreozay@mail.com>
2022-09-29 17:01:31 +0300

    +added: basic level generator

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLevelData", menuName = "Levels/New Level")]
+
+public class Level : ScriptableObject
+{
+    public GameObject levelBase;
+    public GameObject collectableObject;
+
+    public CollectableObjectType collectableObjectType;
+    public CollectableObjectShape collectableObjectShape;
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private Level level;
+    private List<GameObject> collectableObjects;
+
+    void Start()
+    {
+        Instantiate(level.levelBase);
+        
+        Instantiate(level.levelBase, OffsetBetweenBases(), Quaternion.identity);
+        
+        GetCollectableObjects();
+    }
+
+    private void GetCollectableObjects()
+    {
+        collectableObjects = new List<GameObject>();
+
+        collectableObjects = Resources.LoadAll<GameObject>("CollectableObjects").ToList();
+
+        foreach (var item in collectableObjects)
+        {
+            if (item.name.ToLowerInvariant() == (level.collectableObjectType.ToString() + level.collectableObjectShape.ToString()).ToLowerInvariant())
+            {
+                Instantiate(item);
+            }
+        }
+    }
+
+    private Vector3 OffsetBetweenBases()
+    {
+        int childCount = level.levelBase.transform.GetChild(0).childCount + 1;
+
+        Vector3 offset = level.levelBase.transform.GetChild(0).GetChild(0).GetComponent<Renderer>().bounds.size * childCount;
+        offset = new Vector3(0, 0, offset.z);
+        
+        return offset;
+    }
+}
+
+public enum CollectableObjectType
+{
+    CUBE,
+    SPHERE,
+    CAPSULE
+}
+
+public enum CollectableObjectShape
+{
+    ARROW,
+    TRIANGLE,
+    RECTANGLE
+}
